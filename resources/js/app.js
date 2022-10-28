616cee8c7da8109514a6a6d438106439c16ed0e5
YuNandarMoe31 <yunandarmoe31@gmail.com>
2022-10-28 23:51:24 +0630

    Vue Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue';
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
+import ProductComponent from './components/ProductComponent.vue';
+
 app.component('example-component', ExampleComponent);
+app.component('product-component', ProductComponent);
 
 app.mount('#app');
